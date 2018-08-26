c057046c2608b0bed8b94457e3fa8c6a8372d84f
kshitiz <kshitizmaharjan@gmail.com>
2018-08-26 22:45:12 +0545

    Index file broken into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherInfoComponent } from './weather-info/weather-info.component';
+import { ForecastDetailComponent } from './forecast-detail/forecast-detail.component';
+import { FormSearchComponent } from './form-search/form-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherInfoComponent,
+    ForecastDetailComponent,
+    FormSearchComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/forecast-detail/forecast-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forecast-detail',
+  templateUrl: './forecast-detail.component.html',
+  styleUrls: ['./forecast-detail.component.css']
+})
+export class ForecastDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
